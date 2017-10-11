5de1f676bb2e59bfdcc7224cb41eb2c4c0bc4978
datafrost1997 <aj.nataraj2604@gmail.com>
2017-10-11 13:17:20 +0530

    loginActivity file Created

---
+++ app/src/main/java/com/devslopes/datafrost1997/gitterchat/loginActivity.kt
@@ -0,0 +1,12 @@
+package com.devslopes.datafrost1997.gitterchat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class loginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
