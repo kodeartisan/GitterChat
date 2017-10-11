e629e18e72209b6763b00edf66922766efe0845c
datafrost1997 <aj.nataraj2604@gmail.com>
2017-10-11 16:40:12 +0530

    User Profile Build

--- app/src/main/java/com/devslopes/datafrost1997/gitterchat/CreateUserActivity.kt
+++ app/src/main/java/com/devslopes/datafrost1997/gitterchat/CreateUserActivity.kt
@@ -1,5 +1,6 @@
 package com.devslopes.datafrost1997.gitterchat
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -17,6 +18,7 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View) {
+
         val random = Random()
         val color = random.nextInt(2)
         val avatar = random.nextInt(28)
@@ -33,6 +35,18 @@ class CreateUserActivity : AppCompatActivity() {
 
     fun generateColorClicked(view: View) {
 
+        val random =Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB]"
     }
 
     fun createUserClicked(view: View) {
