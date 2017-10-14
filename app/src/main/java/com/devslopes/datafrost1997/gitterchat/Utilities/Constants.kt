b789e9967f6c8cffcaafdcb7dca916dc0ee9713c
datafrost1997 <aj.nataraj2604@gmail.com>
2017-10-14 17:17:08 +0530

    Modified AuthService and Constants file

--- app/src/main/java/com/devslopes/datafrost1997/gitterchat/Utilities/Constants.kt
+++ app/src/main/java/com/devslopes/datafrost1997/gitterchat/Utilities/Constants.kt
@@ -4,6 +4,7 @@ package com.devslopes.datafrost1997.gitterchat.Utilities
  * Created by datafrost1997 on 13/10/17.
  */
 
-const val BASE_URL = "http://localhost:3005/v1"
+//const val BASE_URL = " http://10.0.2.2:3005/v1"
+const val BASE_URL = " https://gitterchat.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 
