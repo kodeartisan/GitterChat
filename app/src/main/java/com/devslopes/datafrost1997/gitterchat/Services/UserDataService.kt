431262abfdb609a0bb31b002d8095d5c2b054811
datafrost1997 <aj.nataraj2604@gmail.com>
2017-10-15 13:26:33 +0530

    Modified AuthService and Constants file

---
+++ app/src/main/java/com/devslopes/datafrost1997/gitterchat/Services/UserDataService.kt
@@ -0,0 +1,13 @@
+package com.devslopes.datafrost1997.gitterchat.Services
+
+/**
+ * Created by datafrost1997 on 15/10/17.
+ */
+object UserDataService {
+
+    var id = ""
+    var avatarColor = ""
+    var avatarName = ""
+    var email = ""
+    var name = ""
+}
