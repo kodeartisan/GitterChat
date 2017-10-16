e50406f7efadef8d4ebb52e26932195b7d2df22f
datafrost1997 <aj.nataraj2604@gmail.com>
2017-10-16 22:45:05 +0530

    MessageService Modified

--- app/src/main/java/com/devslopes/datafrost1997/gitterchat/Services/MessageService.kt
+++ app/src/main/java/com/devslopes/datafrost1997/gitterchat/Services/MessageService.kt
@@ -1,6 +1,11 @@
 package com.devslopes.datafrost1997.gitterchat.Services
 
+import android.content.Context
+import android.util.Log
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
 import com.devslopes.datafrost1997.gitterchat.Model.Channel
+import com.devslopes.datafrost1997.gitterchat.Utilities.URL_GET_CHANNELS
 
 /**
  * Created by datafrost1997 on 16/10/17.
@@ -8,4 +13,22 @@ import com.devslopes.datafrost1997.gitterchat.Model.Channel
 object MessageService {
 
     val channels = ArrayList<Channel> ()
+
+    fun getChannels(context: Context, complete: (Boolean) -> Unit) {
+        val channelsRequest = object : JsonArrayRequest(Method.GET, URL_GET_CHANNELS, null , Response.Listener {
+
+        }, Response.ErrorListener { error ->
+            Log.d("ERROR", "Couldn't Retrieve Channels")
+            complete(false)
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer ${AuthService.authToken}")
+                return headers
+            }
+        }
+    }
 }
