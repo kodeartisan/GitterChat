1979edf2f026ea8226c54255a84fa5e6e52eb2f3
datafrost1997 <aj.nataraj2604@gmail.com>
2017-10-28 21:11:50 +0530

    DownloadMessages Committed

--- app/src/main/java/com/devslopes/datafrost1997/gitterchat/Controller/MainActivity.kt
+++ app/src/main/java/com/devslopes/datafrost1997/gitterchat/Controller/MainActivity.kt
@@ -154,31 +154,38 @@ class MainActivity : AppCompatActivity() {
 
     private val onNewChannel = Emitter.Listener { args ->
 //        println(args[0] as String)
-        runOnUiThread {
-            val channelName = args[0] as String
-            val channelDescription = args[1] as String
-            val channelId =args[2] as String
+        if(App.prefs.isLoggedIn){
+            runOnUiThread {
+                val channelName = args[0] as String
+                val channelDescription = args[1] as String
+                val channelId =args[2] as String
 
-            val newChannel = Channel(channelName, channelDescription, channelId)
-            MessageService.channels.add(newChannel)
-            channelAdapter.notifyDataSetChanged()
+                val newChannel = Channel(channelName, channelDescription, channelId)
+                MessageService.channels.add(newChannel)
+                channelAdapter.notifyDataSetChanged()
+            }
         }
+
     }
 
     private val onNewMessage = Emitter.Listener { args ->
-        runOnUiThread {
-            val msgBody = args[0] as String
-            val channelId = args[2] as String
-            val userName = args[3] as String
-            val userAvatar = args[4] as String
-            val userAvatarColor = args[5] as String
-            val id = args[6] as String
-            val timeStamp = args[7] as String
+        if(App.prefs.isLoggedIn) {
+            runOnUiThread {
+                val channelId = args[2] as String
+                if(channelId == selectedChannel?.id) {
+                    val msgBody = args[0] as String
+                    val userName = args[3] as String
+                    val userAvatar = args[4] as String
+                    val userAvatarColor = args[5] as String
+                    val id = args[6] as String
+                    val timeStamp = args[7] as String
 
-            val newMessage = Message(msgBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
-            MessageService.messages.add(newMessage)
-            println(newMessage.message)
+                    val newMessage = Message(msgBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
+                    MessageService.messages.add(newMessage)
+                }
+            }
         }
+
     }
     fun sendMesgBtnClicked(view: View) {
         if (App.prefs.isLoggedIn && messageTextField.text.isNotEmpty() && selectedChannel != null) {
--- app/src/main/java/com/devslopes/datafrost1997/gitterchat/Services/MessageService.kt
+++ app/src/main/java/com/devslopes/datafrost1997/gitterchat/Services/MessageService.kt
@@ -9,6 +9,7 @@ import com.devslopes.datafrost1997.gitterchat.Controller.App
 import com.devslopes.datafrost1997.gitterchat.Model.Channel
 import com.devslopes.datafrost1997.gitterchat.Model.Message
 import com.devslopes.datafrost1997.gitterchat.Utilities.URL_GET_CHANNELS
+import com.devslopes.datafrost1997.gitterchat.Utilities.URL_GET_MESSAGES
 import org.json.JSONException
 
 /**
@@ -52,4 +53,44 @@ object MessageService {
         }
         App.prefs.requestQueue.add(channelsRequest)
     }
+
+    fun getMessages(channelId: String, complete: (Boolean) -> Unit) {
+
+        val url = "$URL_GET_MESSAGES$channelId"
+        val messagesRequest = object : JsonArrayRequest(Method.GET, url, null, Response.Listener { response ->
+
+        try {
+          for (x in 0 until response.length()) {
+              val message = response.getJSONObject(x)
+              val messageBody = message.getString("messageBody")
+              val channelId = message.getString("channelId")
+              val id = message.getString("id")
+              val userName = message.getString("userName")
+              val userAvatar = message.getString("userAvatar")
+              val userAvatarColor = message.getString("userAvatarColor")
+              val timeStamp = message.getString("timeStamp")
+
+              val newMessage = Message(messageBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
+              this.messages.add(newMessage)
+          }
+            complete(true)
+        } catch (e: JSONException) {
+            Log.d("JSON", "EXC: "  + e.localizedMessage)
+            complete(false)
+        }
+
+        }, Response.ErrorListener {
+            Log.d("ERROR", "Couldn't Retrieve Channels")
+            complete(false)
+        }) {
+        override fun getBodyContentType(): String {
+            return "application/json; charset=utf-8"
+        }
+        override fun getHeaders(): MutableMap<String, String> {
+            val headers = HashMap<String, String>()
+            headers.put("Authorization", "Bearer ${App.prefs.authToken}")
+            return headers
+        }
+    }
+        App.prefs.requestQueue.add(messagesRequest)
 }
